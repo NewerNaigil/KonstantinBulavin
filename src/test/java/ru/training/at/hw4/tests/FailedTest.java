829448ccee56ac7cb7fde91e3d32b36a7285e674
Konstantin Bulavin <konstantin.bulavin.job@gmail.com>
2021-06-30 15:54:54 +0300

    FailedTest

--- src/test/java/ru/training/at/hw4/tests/FailedTest.java
+++ src/test/java/ru/training/at/hw4/tests/FailedTest.java
@@ -2,9 +2,12 @@ package ru.training.at.hw4.tests;
 
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import ru.training.at.hw4.Tags;
+import ru.training.at.hw4.listeners.AllureListener;
 
+//@Listeners(AllureListener.class)
 public class FailedTest extends AbstractTest {
 
     @Feature(value = "Test UI interface")
@@ -15,35 +18,32 @@ public class FailedTest extends AbstractTest {
         // 1. Open test site by URL
         actionStep.openHomePage();
 
-        // 2. Assert Browser title
-        assertionStep.assertBrowserTitle("Failed");
-
         // 3. Perform login
-        actionStep.login(configFileReader.getLogin(), configFileReader.getPassword());
+        actionStep.login("Vadim", "1234");
 
         // 4. Assert Username is loggined
         assertionStep.usernameIsLoggined();
-
-        // 5. Assert that there are 4 items on the header section are displayed and they have proper texts
-        assertionStep.headerItemsDisplayedAndHaveProperTexts();
-
-        // 6. Assert that there are 4 images on the Index Page and they are displayed
-        assertionStep.fourImagesOnIndexPage();
-
-        // 7. Assert that there are 4 texts on the Index Page under icons and they have proper text
-        assertionStep.textsUnderIconsHaveProperText();
-
-        // 8. Assert that there is the iframe with “Frame Button” exist
-        assertionStep.iframeWithFrameButtonExist();
-
-        // 9. Switch to the iframe and check that there is “Frame Button” in the iframe
-        // 10. Switch to original window back
-        assertionStep.frameButtonInIframeExist();
-
-        // 11. Assert that there are 5 items in the Left Section are displayed and they have proper text
-        assertionStep.fiveItemsInLeftSectionDisplayedAndHaveProperText();
-
-        // 12. Close Browser
-        // Browser will close in AbstractTest
+        //
+        //        // 5. Assert that there are 4 items on the header section are displayed and they have proper texts
+        //        assertionStep.headerItemsDisplayedAndHaveProperTexts();
+        //
+        //        // 6. Assert that there are 4 images on the Index Page and they are displayed
+        //        assertionStep.fourImagesOnIndexPage();
+        //
+        //        // 7. Assert that there are 4 texts on the Index Page under icons and they have proper text
+        //        assertionStep.textsUnderIconsHaveProperText();
+        //
+        //        // 8. Assert that there is the iframe with “Frame Button” exist
+        //        assertionStep.iframeWithFrameButtonExist();
+        //
+        //        // 9. Switch to the iframe and check that there is “Frame Button” in the iframe
+        //        // 10. Switch to original window back
+        //        assertionStep.frameButtonInIframeExist();
+        //
+        //        // 11. Assert that there are 5 items in the Left Section are displayed and they have proper text
+        //        assertionStep.fiveItemsInLeftSectionDisplayedAndHaveProperText();
+        //
+        //        // 12. Close Browser
+        //        // Browser will close in AbstractTest
     }
 }
