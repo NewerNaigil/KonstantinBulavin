d7d39bffebecf748255b55b347542e3084f1fd42
Konstantin Bulavin <konstantin.bulavin.job@gmail.com>
2021-07-01 17:33:11 +0300

    Fix encoding problem

--- src/main/java/ru/training/at/hw4/ProperTestData.java
+++ src/main/java/ru/training/at/hw4/ProperTestData.java
@@ -3,14 +3,16 @@ package ru.training.at.hw4;
 import java.util.Set;
 
 public class ProperTestData {
+
     public static final Set<String> HEADER_SELECTOR_ITEMS_SET =
         Set.of("HOME", "CONTACT FORM", "SERVICE", "METALS & COLORS");
 
     public static final Set<String> TEXT_UNDER_ICON_SET =
         Set.of("To include good practices\nand ideas from successful\nEPAM project",
             "To be multiplatform",
-            "Already have good base\n(about 20 internal and\nsome external projects),\nwish to get more…",
+            "Already have good base\n(about 20 internal and\nsome external projects),\nwish to get more" + "…",
             "To be flexible and\ncustomizable");
+
 
     public static final Set<String> TEXT_LEFT_SECTION_SET =
         Set.of("Home", "Contact form", "Service", "Metals & Colors", "Elements packs");
