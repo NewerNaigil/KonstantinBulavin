94688d133c52b11d05ddb356fe95738af3f4051d
Konstantin Bulavin <konstantin.bulavin.job@gmail.com>
2021-07-01 14:12:01 +0300

    Made changes from HW3

--- src/main/java/ru/training/at/hw4/page/components/HeaderMenu.java
+++ src/main/java/ru/training/at/hw4/page/components/HeaderMenu.java
@@ -9,19 +9,27 @@ import org.openqa.selenium.support.FindBy;
 
 public class HeaderMenu extends AbstractComponent {
 
-    @FindBy(xpath = "//a[contains(text(), 'Home') "
-        + "or contains(text(), 'Contact form') "
-        + "or contains(text(), 'Service') "
-        + "or contains(text(), 'Metals & Colors')]")
-    private List<WebElement> headMenuItems;
+    @FindBy(css = "a[href = 'index.html']")
+    private WebElement home;
+    @FindBy(css = "a[href = 'contacts.html']")
+    private WebElement contactForm;
+    @FindBy(css = ".dropdown-toggle")
+    private WebElement service;
+    @FindBy(css = "a[href = 'metals-colors.html']")
+    private WebElement metalAndColors;
+    @FindBy(css = "a[href = 'different-elements.html']")
+    private WebElement differentElements;
 
     public HeaderMenu(WebDriver webDriver) {
         super(webDriver);
     }
 
     public Set<String> getSetOfTextHeadMenuItem() {
-        return headMenuItems.stream()
-                            .map(WebElement::getText)
-                            .collect(Collectors.toSet());
+        return Set.of(home.getText(), contactForm.getText(), service.getText(), metalAndColors.getText());
+    }
+
+    public void clickToDifferentElements() {
+        service.click();
+        differentElements.click();
     }
 }
